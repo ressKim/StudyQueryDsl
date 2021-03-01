0496f5c6e98f27d9af293e8225b3caf3e5714897
ressKim <sh21dlat@gmail.com>
2021-03-01 16:56:12 +0900

    예제 도메인 추가 및 확인

---
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,47 @@
+package study.querydsl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.swing.text.html.parser.Entity;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    EntityManager em;
+
+    @BeforeEach
+    public void before(){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+        //초기화
+
+        em.flush();
+        em.clear();
+    }
+
+    @Test//먼저 jpql 으로 작성
+    public void startJPQL(){
+        //member
+    }
+
+
+}
