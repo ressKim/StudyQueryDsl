e1f6e92aeb0faa7fc28b46a6bb3fcf37afacf7c9
ressKim <sh21dlat@gmail.com>
2021-03-02 20:37:27 +0900

    동적쿼리 최적화 조회 - Builder use

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -5,7 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 
@@ -16,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-class MemberJpaRepositoryTest  {
+class MemberJpaRepositoryTest {
 
     @Autowired
     EntityManager em;
@@ -25,7 +28,7 @@ class MemberJpaRepositoryTest  {
     MemberJpaRepository memberJpaRepository;
 
     @Test
-    public void basicTest(){
+    public void basicTest() {
         Member member = new Member("member1", 10);
         memberJpaRepository.save(member);
 
@@ -40,7 +43,7 @@ class MemberJpaRepositoryTest  {
     }
 
     @Test
-    public void basicQuerydslTest(){
+    public void basicQuerydslTest() {
 
         Member member = new Member("member1", 10);
         memberJpaRepository.save(member);
@@ -56,5 +59,34 @@ class MemberJpaRepositoryTest  {
 
     }
 
+    @Test
+    public void searchTest() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+        //동적쿼리에는 limit 가 있는게 좋다.
+        MemberSearchCondition condition = new MemberSearchCondition();
+//        condition.setAgeGoe(20);
+//        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+        assertThat(result).extracting("username").containsExactly("member3","member4");
+
+
+    }
 
 }
